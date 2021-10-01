c52c10f7bf0558fc4ea8f17e6e74136dd2456969
Chris Birk <cmbirk@gmail.com>
2021-10-01 16:54:30 -0500

    re-add type merging for DefaultTheme type

--- src/Theme/styled.d.ts
+++ src/Theme/styled.d.ts
@@ -1,5 +1,23 @@
 import 'styled-components'
 
 declare module 'styled-components' {
-
+  export interface DefaultTheme extends Record<string, unknown> {
+    colors: {
+      lightGray: string
+      gray: string
+      darkGray: string
+      header: string
+    },
+    components: {
+      DataTableStyles: Record<string, unknown>
+    },
+    shadow?: string,
+    screens: {
+      sm: string
+      md: string
+      lg: string
+      xl: string
+      '2xl': string
+    },
+  }
 }
