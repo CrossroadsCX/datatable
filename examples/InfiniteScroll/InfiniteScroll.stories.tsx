87cd7255dd78e1a1b0f5cb947df59f7f9cd1e669
Chris Birk <cmbirk@gmail.com>
2021-10-20 14:46:54 -0500

    add InfiniteScroll example

---
+++ examples/InfiniteScroll/InfiniteScroll.stories.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo, useRef, useState } from 'react'
+import { Story, Meta } from '@storybook/react'
+import { Column } from 'react-table'
+
+import { createPeople } from '../utils'
+import type { Person } from '../utils'
+
+import { DataTable, DataTableProps, HandleFetchDataArgs } from '../../src/DataTable'
+import { isEmpty, sortBy } from 'lodash'
+
+export default {
+  title: 'examples/InfiniteScroll',
+  component: DataTable,
+} as Meta
+
+const columns: Column<Person>[] = [
+  {
+    Header: 'First Name',
+    accessor: 'firstName',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Age',
+    accessor: 'age'
+  }
+]
+
+const Template: Story<DataTableProps<Person>> = (args) => {
+  const [people, setPeople] = useState(createPeople(10))
+
+  const initialData = useMemo(() => people, [people])
+
+  // We will append new data on fetching
+  const handleFetchData = async ({ pageIndex, pageSize, sortBy: sortByInput }: HandleFetchDataArgs<Person>) => {
+    console.log('Paging:')
+    console.log(pageIndex, pageSize, sortBy)
+    const newPeople = createPeople(pageSize)
+
+    if (!isEmpty(sortByInput)) {
+      const [sortObject] = sortByInput
+      const { id: sortKey } = sortObject
+
+      sortBy(newPeople as Array<Person>, [sortKey])
+    }
+
+    setPeople(newPeople)
+  }
+
+  return (<DataTable<Person> data={initialData} handleFetchData={handleFetchData} {...args} />)
+}
+
+export const AsyncTable = Template.bind({})
+
+AsyncTable.args = {
+  columns,
+  paginated: 'scroll',
+}
