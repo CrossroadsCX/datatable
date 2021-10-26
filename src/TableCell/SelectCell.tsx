13c213f7f0b87e23ded806c12de6be5fa9367d8c
Emmaalara <emmaj1653@gmail.com>
2021-10-25 18:43:57 -0600

    Select styles were added
    Select styles were added

--- src/TableCell/SelectCell.tsx
+++ src/TableCell/SelectCell.tsx
@@ -6,6 +6,38 @@ type SelectCellProps = {
   handleChange: (value: any) => void
 }
 
+const customStyles = {
+  menu: () => ({
+    width: 150,
+    color: '#000',
+    padding: 1,
+    background: '#fff',
+    fontSize: 12,
+  }),
+
+  control: () => ({
+    width: 150,
+    height: 25,
+    display: 'flex',
+    color: '#000'
+  }),
+
+  indicatorSeparator: () => ({
+    display: 'none'
+  }),
+
+  indicatorsContainer: () => ({
+    display: 'none'
+  }),
+
+  singleValue: () => {
+    const color = '#000'
+    const opacity = 1;
+    const transition = 'opacity 300ms';
+    return { opacity, transition, color };
+  }
+}
+
 export const SelectCell: React.FC<SelectCellProps> = ({ handleChange, options }) => {
   const onChange = (value: OptionTypeBase | null) => {
     handleChange(value);
@@ -17,6 +49,8 @@ export const SelectCell: React.FC<SelectCellProps> = ({ handleChange, options })
       onChange={onChange}
       menuPortalTarget={document.body}
       menuPosition="fixed"
+      styles={customStyles}
+      className='border-0 border-b border-blue-400 border-solid'
     />
   );
 };
